f72397242dce329e1d6fd22ca99a6e2c9786e73a
Francisco Ramos <jorge.ramos.francisco.15@gmail.com>
2022-12-22 20:59:43 -0300

    Fix project creation endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,9 +3,6 @@ import { admin, functions, db } from "./tools/firebase";
 // admin.initializeApp();
 db.settings({ ignoreUndefinedProperties: true });
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
 export const apiprojects = functions.https.onRequest(async (req, res) => {
   await (await import("./projects/projects.api")).app(req, res);
 });
--- functions/src/projects/projects.service.ts
+++ functions/src/projects/projects.service.ts
@@ -1,5 +1,5 @@
 import { Project } from "../models/project.model";
-import { addDocCol } from "./../../src/services/db.service";
+import { addDocCol } from "./../services/db.service";
 
 export const createProject = async (owner: string, data: Project) => {
   return addDocCol(`users/${owner}/projects`, data);
