f6590957421617280a51d16bb79294c9ca745f2b
Francisco Ramos <jorge.ramos.francisco.15@gmail.com>
2023-01-07 09:42:53 -0300

    FIX Task id creation & tADD Task edition

--- functions/src/projects/projects.api.ts
+++ functions/src/projects/projects.api.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import { logger } from "./../tools/firebase";
 import { GoalCreation, ProjectCreation } from "../models/project.model";
 import { authVerification } from "../tools/middlewares/auth-validation";
-import { createBoard, createGoal, createProject, deactivateProject, editProject, createTask } from "./projects.service";
+import { createBoard, createGoal, createProject, deactivateProject, editProject, createTask, editTask } from "./projects.service";
 import * as cors from "cors";
 import { arrayUnion, getDoc } from "../services/db.service";
 import { userBelogsTo } from "../services/general.service";
@@ -185,6 +185,41 @@ app.post("/api/v1/createTask", async (req, res) => {
     if (!creationResp.success) {
       logger.error(creationResp.message);
       return res.status(500).send({ success: false, message: creationResp.message }).end();
+    }
+    return res.status(200).send({ success: true, data: finalData }).end();
+  } catch (error) {
+    logger.error(error);
+    return res.status(500).send({ success: false }).end();
+  }
+});
+
+app.post("/api/v1/editTask", async (req, res) => {
+  try {
+    const body = req.body;
+    const rawTaskData = body?.taskData;
+    const boardId = body?.boardId;
+    const goalId = body?.goalId;
+    const taskId = body?.taskId;
+    const projectInfo = body?.projectInfo;
+    const user = res.locals.user;
+
+    // FIXME: There should be project shared system comprobation instead
+    const goalData = await getDoc(`users/${projectInfo.owner.id}/projects/${projectInfo.id}/goals/${goalId}`);
+    if (!goalData.exists || goalData?.success === false) {
+      return res.status(500).send({ success: false, message: "Problem detected with the goal" }).end();
+    }
+
+    // Filter data
+    // TODO: Add type
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const finalData: any = {
+      name: rawTaskData?.name,
+      active: rawTaskData?.active ?? true,
+    };
+    const response = await editTask(projectInfo.owner.id, projectInfo.id, goalId, boardId, taskId, finalData, user.uid);
+    if (!response.success) {
+      logger.error(response.message);
+      return res.status(500).send({ success: false, message: response.message }).end();
     }
     return res.status(200).send({ success: true, data: finalData }).end();
   } catch (error) {
--- functions/src/projects/projects.service.ts
+++ functions/src/projects/projects.service.ts
@@ -1,7 +1,7 @@
 import { GoalCreation, ProjectCreation, ProjectOwner } from "../models/project.model";
 import { uploadByString } from "../services/storage.service";
 import { db, serverTimestamp } from "../tools/firebase";
-import { addDocCol, arrayUnion, updateDoc } from "./../services/db.service";
+import { addDocCol, arrayUnion, getDoc, updateDoc } from "./../services/db.service";
 
 export const createProject = async (owner: ProjectOwner, data: ProjectCreation) => {
   const { description, ...dbData } = data;
@@ -66,8 +66,46 @@ export const createBoard = (owner: string, projId: string, goalId: string, data:
 // TODO: Add type for data
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const createTask = (owner: string, projId: string, goalId: string, boardId: string, data: any, userId: string) => {
+  const taskDocRef = db.collection(`users/${owner}/projects/${projId}/goals/${goalId}/boards/${boardId}/tasks`).doc();
+  const taskId = taskDocRef.id;
+
+  const finalData = {
+    ...data,
+    id: taskId,
+  };
+
   const dbData = {
-    tasks: arrayUnion({ ...data }),
+    tasks: arrayUnion(finalData),
+    updatedAt: serverTimestamp(),
+    updatedBy: userId,
+  };
+
+  return Promise.all([
+    addDocCol(`users/${owner}/projects/${projId}/goals/${goalId}/boards/${boardId}/tasks`, {}, taskId),
+    updateDoc(`users/${owner}/projects/${projId}/goals/${goalId}/boards/${boardId}`, dbData),
+  ]).then((resp) => resp[0]).catch((error) => ({ success: false, message: error.message ?? error }));
+};
+
+// TODO: Add type for data
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const editTask = async (owner: string, projId: string, goalId: string, boardId: string, taskId: string, data: any, userId: string) => {
+  const boardPath = `users/${owner}/projects/${projId}/goals/${goalId}/boards/${boardId}`;
+
+  // TODO: Think about managing an object with the tasks instead of an array
+  // This will fix editing the whole array of tasks
+  const boardData = await getDoc(boardPath);
+  const boardTasks = boardData.tasks;
+
+  if (!boardTasks) return ({ success: false, message: "No tasks to edit" });
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const editingTaskIndex = boardTasks.findIndex((task: any) => task.id === taskId);
+  if (editingTaskIndex < 0) return ({ success: false, message: "Task not found" });
+
+  boardTasks[editingTaskIndex] = { ...boardTasks[editingTaskIndex], ...data };
+
+  const dbData = {
+    tasks: boardTasks,
     updatedAt: serverTimestamp(),
     updatedBy: userId,
   };
