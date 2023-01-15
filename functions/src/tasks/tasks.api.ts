8e8878bf4cdc56911e73c6726c151b4d84a8011d
Francisco Ramos <jorge.ramos.francisco.15@gmail.com>
2023-01-15 09:08:24 -0300

    FIX Api endpoints HTTP methods

--- functions/src/tasks/tasks.api.ts
+++ functions/src/tasks/tasks.api.ts
@@ -44,7 +44,7 @@ app.post("/api/v1/create", async (req, res) => {
   }
 });
 
-app.post("/api/v1/edit", async (req, res) => {
+app.patch("/api/v1/edit", async (req, res) => {
   try {
     const body = req.body;
     const rawTaskData = body?.taskData;
